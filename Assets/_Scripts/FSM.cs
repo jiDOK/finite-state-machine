0667c45dd6e2c33142e473bc74e348af506e5971
jiDOK <jan.ahrens@posteo.de>
2017-09-27 13:20:25 +0200

    first commit

---
+++ Assets/_Scripts/FSM.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM : MonoBehaviour
+{
+    public enum Season { Spring, Summer, Fall, Winter};
+    public Season season = Season.Spring;
+
+    public delegate void SeasonChangedHandler(Season season);
+    public event SeasonChangedHandler SeasonChanged;
+
+    float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2f)
+        {
+            timer = 0f;
+            ChangeSeason();
+        }
+    }
+
+    void ChangeSeason()
+    {
+        switch (season)
+        {
+            case Season.Spring:
+                season = Season.Summer;
+                break;
+            case Season.Summer:
+                season = Season.Fall;
+                break;
+            case Season.Fall:
+                season = Season.Winter;
+                break;
+            case Season.Winter:
+                season = Season.Spring;
+                break;
+            default:
+                break;
+        }
+        if (SeasonChanged != null)
+        {
+            SeasonChanged(season);
+        }
+    }
+}
---
+++ Assets/_Scripts/GameDev.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class GameDev : MonoBehaviour
+{
+    public FSM seasonFSM;
+
+    void OnEnable()
+    {
+        seasonFSM.SeasonChanged += OnSeasonChanged;
+        seasonFSM.SeasonChanged += Complain;
+    }
+    void OnDisable()
+    {
+        seasonFSM.SeasonChanged -= OnSeasonChanged;
+        seasonFSM.SeasonChanged -= Complain;
+    }
+
+    void Complain(FSM.Season season)
+    {
+        if (season == FSM.Season.Winter)
+        {
+            Debug.Log("I hate snow!"); 
+        }
+    }
+
+    public void OnSeasonChanged(FSM.Season season)
+    {
+        switch (season)
+        {
+            case FSM.Season.Spring:
+                Debug.Log("Developing!");
+                break;
+            case FSM.Season.Summer:
+                Debug.Log("Vacation!");
+                break;
+            case FSM.Season.Fall:
+                Debug.Log("Buy coal...");
+                break;
+            case FSM.Season.Winter:
+                Debug.Log("Brrrrrrrr!");
+                break;
+            default:
+                break;
+        }
+    }
+}
